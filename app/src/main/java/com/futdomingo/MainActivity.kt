54445b3fcbfeaa9443c1c5e3cfe7baf452f5301f
Yan <yan.andrade.ds@gmail.com>
2024-02-19 16:37:32 -0300

    increased validation to inputs, partial working

--- app/src/main/java/com/futdomingo/MainActivity.kt
+++ app/src/main/java/com/futdomingo/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -19,12 +33,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection.Companion.In
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -64,14 +83,6 @@ class MainActivity : ComponentActivity()
 
     }
 }
-
-
-@Preview
-@Composable
-fun PreviewApp(){
-    App(null,TeamsDrawnViewModel())
-}
-
 
 @Composable
 fun App(mNavController: NavController?, drawnViewModel: TeamsDrawnViewModel?)
@@ -142,22 +153,53 @@ fun DrawButton(teams: List<String>,
 
 @Composable
 fun InputCenterComponents(show: Boolean, inputPlayers: (String) -> Unit, modifier: Modifier) {
-    val typedText = remember {
-        mutableStateOf("")
-    }
+
+    val typedText = remember { mutableStateOf("") }
+    var invalidCharacters by remember {  mutableStateOf(false) }
+    var count  by remember { mutableIntStateOf(0) }
+    val limit = 20
+
+
+
+
 
      if (show) {
 
-         OutlinedTextField(
-             value = typedText.value,
-             onValueChange = { newText -> typedText.value = newText },
-             modifier = modifier,
-         )
 
-         Button(onClick = { inputPlayers(typedText.value) },
-             modifier = modifier.offset(0.dp,56.dp)) {
-                Text(text = "Inserir")
-         }
+             Surface(modifier = modifier.wrapContentSize(), shadowElevation = 8.dp) {
+                Column {
+                    OutlinedTextField(
+                        value = typedText.value,
+                        onValueChange = { typedText.value = it
+                            invalidCharacters = isInvalidInput(it)
+                            count = it.length},
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text, autoCorrect = false),
+                        label = { Text(text = "Nome")},
+                        isError = invalidCharacters,
+                        trailingIcon = { if(invalidCharacters) Icon(Icons.Filled.Info,  "") },
+                        singleLine = true,
+                        supportingText = {
+                            if(invalidCharacters) Text("Dígito inválido", modifier = Modifier.align(Alignment.End))
+                            else Text("$count/$limit") }
+                    )
+
+                    Button(
+                        onClick = { if(!invalidCharacters) {
+                            inputPlayers(typedText.value)
+                            typedText.value = "" }
+                        }
+
+                    ) {
+                        Text(text = "Inserir")
+                    }
+                }
+
+             }
+
+
+
+
+
      }
 
 }
@@ -192,4 +234,13 @@ fun drawTeams(playerList: List<String>): List<String> {
     return drawnList
 }
 
+private fun isInvalidInput(input: String): Boolean {
+    val pattern = "[^a-zA-z]"
+    val regex = Regex(pattern)
 
+    input.forEach {
+        if (regex.matches(it.toString())) return true
+    }
+
+    return false
+}
